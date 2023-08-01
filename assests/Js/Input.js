1992889ae477154d1dd48aefc2962e3cef7a849c
wfpinedaf <wfpinedaf@outlook.com>
2023-07-31 19:07:26 -0500

    last updates

--- assests/Js/Input.js
+++ assests/Js/Input.js
@@ -35,4 +35,26 @@
   };
 })();
 
-inputNumber($(".input-number"));
+// inputNumber($(".input-number"));
+
+/* Creacion del Log In
+
+User_id: Admin
+Password: Admin123
+
+Botno debera tener algo asi 
+<button onclick="window.location.href='/page2'">Continue</button>
+*/
+
+// let credentials = { Username: "Admin", Password: "123" };
+// let EmailInput = document.querySelector("#form2Example1");
+// let PassInput = document.querySelector("#form2Example2");
+// let LoginAccess = document.querySelector("#BtnIniciar");
+
+// LoginAccess.addEventListener("click", (e) => {
+//   e.preventDefault();
+// });
+
+// if (EmailInput.value = credentials.Username && PassInput.value =)
+
+// console.log(credentials.Username);
