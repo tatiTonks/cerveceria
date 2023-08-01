69869321e3c6d711efc40dbfec33af7873adb6fc
mariagriv99 <mariagriv99@gmail.com>
2023-07-31 21:27:31 -0500

    updates despues del mierdero

--- assests/Js/main.js
+++ assests/Js/main.js
@@ -87,3 +87,34 @@ window.addEventListener("scroll", () => {
     });
   }
 });
+
+
+
+/* ALejandra Updates */
+
+const images = [
+  { url: '1.webp', estilo:'background-position-y: 30%', alt: 'foto 1' },
+  { url: '2.webp', estilo: 'background-position-y: 70%',alt: 'foto 2' },
+];
+let previousIndex = -1; // Índice de la imagen anterior
+function getRandomIndex() {
+  let randomIndex = Math.floor(Math.random() * images.length);
+  // Si el índice es igual al índice anterior, elige otro aleatoriamente
+  while (randomIndex === previousIndex) {
+    randomIndex = Math.floor(Math.random() * images.length);
+  }
+  return randomIndex;
+}
+
+function changeBackground() {
+  const randomIndex = getRandomIndex();
+  const selectedImage = images[randomIndex];
+
+
+  let seccionFondos = document.querySelector("#sliderPolas")
+
+  seccionFondos.setAttribute("style", `background-image: url('assets/img/${selectedImage.url}'); ${selectedImage.estilo}`)
+  previousIndex = randomIndex; // Actualiza el índice anterior
+}
+setInterval(changeBackground, 3000);
+changeBackground()
